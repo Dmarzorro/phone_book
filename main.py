dc09b5a7aaa563b48d9dd47bdecacb409c967b0e
Dmarzorro <jaroslawmazur63@gmail.com>
2025-07-07 05:11:29 +0200

    choice handling methods added for menu class: add_contact and delete_contact skeleton

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ class Menu:
 
     def run(self):
         while True:
-            self.show_menu(self)
+            self.show_menu()
+            choice = input("Choose action: ")
+            self.handle_choice(choice)
 
     def show_menu(self):
         print('\n===Welcome to Contact Book ===')
@@ -16,3 +18,27 @@ class Menu:
         print('4. Exit')
 
     choice = input("Choose action: ")
+
+
+    def add_contact(self):
+        first_name = input("First name: ")
+        last_name = input("Last name: ")
+        phone_number = input("Phone number: ")
+        email = input("Email address: ") or None
+
+        contact = Contact(first_name, last_name, phone_number, email)
+        result = self.contact_list.add_contact(contact)
+        print(result)
+
+    def delete_contact(self):
+        if not self.contact_list.contacts:
+            print("No contacts to delete")
+            return
+
+        print("\n Choose contact to delete: ")
+        for idx, contact in enumerate(self.contact_list.contacts, start=1):
+            print(f"{idx}. {contact}")
+
+        try:
+            choice = int(input("Choose contact: "))
+
