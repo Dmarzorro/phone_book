4281bbe204a7c9771f973224b08b0a7a6e3adfc6
Dmarzorro <jaroslawmazur63@gmail.com>
2025-07-07 04:55:11 +0200

    Menu class with basic methods added

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 from book import *
+
+class Menu:
+    def __init__(self):
+        self.contact_list = ContactList()
+
+    def run(self):
+        while True:
+            self.show_menu(self)
+
+    def show_menu(self):
+        print('\n===Welcome to Contact Book ===')
+        print('1. Add Contact')
+        print('2. Edit Contact')
+        print('3. Delete Contact')
+        print('4. Exit')
+
+    choice = input("Choose action: ")
