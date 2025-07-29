54c9d6343dac7b3ebf0476b0e08af613176c9558
Dmarzorro <jaroslawmazur63@gmail.com>
2025-07-29 04:55:54 +0200

    repository.py created and updated with new add_contact function

---
+++ desktop_app/repository.py
@@ -0,0 +1,42 @@
+from peewee import IntegrityError
+from uuid import uuid4, UUID
+from typing import Optional
+
+from desktop_app.helper import ValidationError
+from models import Contact, db
+from helper import (
+    validate_name,
+    validate_phone,
+    validate_email,
+    ValidationError,
+)
+
+def add_contact(first_name: str, last_name: str, phone: str, email: Optional[str] = None) -> Contact:
+    """
+    Adds a new contact with the provided details to the contact list. This operation
+    creates an instance of the `Contact` class using the specified inputs and includes
+    optional handling for additional email addresses if applicable.
+    """
+    #validation
+    first_name = validate_name(first_name)
+    last_name = validate_name(last_name)
+    phone = validate_phone(phone)
+
+    #saving to database
+    if email:
+        email = validate_email(email)
+    try:
+        with db.atomic():
+            contact = Contact.creat(
+                id=uuid4(),
+                first_name=first_name,
+                last_name=last_name,
+                phone_number=phone,
+                email=email,
+            )
+            return contact
+    except IntegrityError as exc:
+        raise ValidationError(f"Contact already exists: {exc}")
+
+def delete_contact(contact_id: UUID) -> bool:
+    
