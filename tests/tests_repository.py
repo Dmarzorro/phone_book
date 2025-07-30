1e9dd2b00e5195d22e42ee2abf184078715e38a7
Dmarzorro <jaroslawmazur63@gmail.com>
2025-07-30 20:51:37 +0200

    add: test_repository  with initial tests for repository.py

---
+++ tests/tests_repository.py
@@ -0,0 +1,30 @@
+import pytest
+from desktop_app.repository import add_contact, delete_contact, get_all_contacts
+from desktop_app.helper import ValidationError
+from desktop_app.models import Contact, db
+from uuid import UUID
+
+def test_add_contact_success(test_db):
+    contact = add_contact(
+        first_name="Yaroslav",
+        last_name="Mazur",
+        phone="+48572430878",
+        email="somethinghere@gmail.com"
+    )
+    assert isinstance(contact, Contact)
+    assert contact.first_name == "Yaroslav"
+    assert contact.last_name == "Mazur"
+    assert contact.phone == "+48572430878"
+    assert contact.email == "somethinghere@gmail.com"
+    assert isinstance(contact.id, UUID)
+
+    contact = get_all_contacts()
+    assert len(contact) == 1
+
+def test_add_contact_phone(test_db):
+    add_contact("Ivan", "Drago", "+48912345678", "somethinghere@gmail.com")
+
+    with pytest.raises(ValidationError):
+        add_contact("Ivan", "Drago", "+48912345678", "somethinghere@gmail.com")
+
+def test_
